07b494d7b598ae1fc33cd0d2536d15711b532a01
enocgirona <92875147+enocgirona@users.noreply.github.com>
2022-02-18 12:43:58 +0100

    Delete M8UF5_js/M8UF5AC41_egirona directory

--- M8UF5_js/M8UF5AC41_egirona/JS/saludarusuari.js
+++
@@ -1,5 +0,0 @@
-function enviarSalutacio() {
-    var nom = document.getElementById('nom')
-    var cognom = document.getElementById('cognom')
-    alert ('HOLA' + nom + cognom + 'GRÀCIES PER OMPLIR EL FORMULARI' )
-}
